824cae8e683e532a4780b4df3874d9deafc166b2
Leonardo P <leonardolpaz95@gmail.com>
2024-12-26 14:37:02 -0300

    feat: add db configuration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getUser(): Promise<string> {
+    const user = await this.usersService.findLastUser();
+    return `Hello ${user.email}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TemporalSdkModule } from './temporal-sdk/temporal-sdk.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Users } from './users/users.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TemporalSdkModule],
+  imports: [
+    TemporalSdkModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'playbookux',
+      database: 'playbookux_development',
+      entities: [Users],
+      synchronize: false,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
